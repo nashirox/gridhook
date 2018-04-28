454a48139fde666417623e8487b7a96b9246b309
Daisuke Nashiro <nashirox@gmail.com>
2018-04-28 10:12:28 +0900

    Change protect_from_forgery option to null session

--- app/controllers/gridhook/events_controller.rb
+++ app/controllers/gridhook/events_controller.rb
@@ -1,5 +1,7 @@
 module Gridhook
   class EventsController < ActionController::Base
+    protect_from_forgery with: :null_session
+
     def create
       Gridhook::Event.process(params)
       head :ok
